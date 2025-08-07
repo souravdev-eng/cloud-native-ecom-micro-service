9083e24d83c488ac1de06b3276b620e63c8f8054
souravdev-eng <souravmajumdar.dev@gmail.com>
2025-08-07 19:05:37 +0530

    Add product card

--- mfe-client/dashboard/src/App.tsx
+++ mfe-client/dashboard/src/App.tsx
@@ -1,8 +1,20 @@
+import ProductCard from './components/ProductCard/ProductCard';
 import './index.css';
 
 const App = () => (
-  <div className='container'>
-    <div>Name: dashboard</div>
+  <div style={{ display: 'flex', gap: 12, flexWrap: 'wrap' }}>
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
+    <ProductCard />
   </div>
 );
 
--- mfe-client/host/src/App.tsx
+++ mfe-client/host/src/App.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
-import './index.css';
+// import './index.css';
 import router from './Router';
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 
