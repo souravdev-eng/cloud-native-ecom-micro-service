ed8f0580f2b969b59cf199df9853f3c5d7381aba
souravdev-eng <souravmajumdar.dev@gmail.com>
2025-08-24 10:54:28 +0530

    Adding a pagination query and sorting logic

--- product/src/models/productModel.ts
+++ product/src/models/productModel.ts
@@ -1,98 +1,95 @@
 import mongoose from 'mongoose';
 
 enum Category {
-    Phone = 'phone',
-    earphone = 'earphone',
-    Book = 'book',
-    Fashions = 'fashions',
-    other = 'other',
+  Phone = 'phone',
+  earphone = 'earphone',
+  Book = 'book',
+  Fashions = 'fashions',
+  other = 'other',
 }
 
 interface ProductAttars {
-    title: string;
-    price: number;
-    image: string;
-    sellerId: string;
-    description: string;
-    quantity?: number;
-    category: Category;
-    tags?: string[];
+  title: string;
+  price: number;
+  image: string;
+  sellerId: string;
+  description: string;
+  quantity?: number;
+  category: Category;
+  tags?: string[];
 }
 
 interface ProductDoc extends mongoose.Document {
-    title: string;
-    price: number;
-    image: string;
-    sellerId: string;
-    description: string;
-    quantity?: number;
-    category: Category;
-    tags?: string[];
+  title: string;
+  price: number;
+  image: string;
+  sellerId: string;
+  description: string;
+  quantity?: number;
+  category: Category;
+  tags?: string[];
 }
 
 interface ProductModel extends mongoose.Model<ProductDoc> {
-    build(attars: ProductAttars): ProductDoc;
+  build(attars: ProductAttars): ProductDoc;
 }
 
 const productSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        image: {
-            type: String,
-            required: true,
-        },
-        category: {
-            type: String,
-            enum: Category,
-            default: Category.other,
-        },
-        description: {
-            type: String,
-        },
-        price: {
-            type: Number,
-            required: true,
-            min: 100,
-        },
-        rating: {
-            type: Number,
-            default: 4.5,
-        },
-        quantity: {
-            type: Number,
-            default: 5,
-        },
-        sellerId: {
-            type: String,
-            required: true,
-        },
-        tags: {
-            type: Array,
-            default: [],
-        },
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            transform(_, ret) {
-                ret.id = ret._id;
-                delete ret._id;
-                delete ret.__v;
-            },
-        },
-    }
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      // enum: Category,
+      default: Category.other,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 100,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    quantity: {
+      type: Number,
+      default: 5,
+    },
+    sellerId: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
 );
 
 productSchema.statics.build = (attars: ProductAttars) => {
-    return new Product(attars);
+  return new Product(attars);
 };
 
-const Product = mongoose.model<ProductDoc, ProductModel>(
-    'Product',
-    productSchema
-);
+const Product = mongoose.model<ProductDoc, ProductModel>('Product', productSchema);
 
 export { Product };
